283d157bf3c4e625d097ba58cc86a37f4fa3e051
wenjunlian <2292082443@qq.com>
2019-06-03 11:30:47 +0800

    1.修改数据，解决冲突，wenjunlian

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText("wenjunlian");
+        tv.setText("wjl");
         tv.setText("123456");
     }
 
